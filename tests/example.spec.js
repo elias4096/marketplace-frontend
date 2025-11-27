bc5de5d2262a31a3f86792bbc8d1f800a532a874
elias <e.detlefsen@student.fontys.nl>
2025-11-27 12:04:14 +0100

    integrated sonarcloud into ci

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -6,17 +6,22 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('http://localhost:5173/');
-
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Marketplace/);
 });
 
 test('get navigation links', async ({ page }) => {
   await page.goto('http://localhost:5173/');
+  await page.getByRole('link', { name: "Upload" }).click();
+  await page.getByRole('link', { name: "Profile" }).click();
+});
 
-  // Click the upload link.
-  await page.getByRole('link', { name: 'Upload' }).click();
+test('get authentication links', async ({ page }) => {
+  await page.goto('http://localhost:5173/profile');
+  await page.getByRole('link', { name: "Don't have an account?" }).click();
+  await page.getByRole('link', { name: "Already have an account?" }).click();
+});
 
-  // Click the profile link.
-  await page.getByRole('link', { name: 'Profile' }).click();
+test('get home link', async ({ page }) => {
+  await page.goto('http://localhost:5173/profile');
+  await page.getByRole('link', { name: "Marketplace" }).click();
 });
