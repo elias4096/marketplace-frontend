b78e747b4cc5664fbde0c0dd0e75a32db76eaafc
elias <e.detlefsen@student.fontys.nl>
2025-11-27 11:20:17 +0100

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -74,7 +74,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev',
-    url: 'http://localhost:4173/',
+    url: 'http://localhost:5173/',
     timeout: 10000
     // reuseExistingServer: !process.env.CI,
   },
